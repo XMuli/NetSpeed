369a4d21edc6d5b9af883c60766bf8fd9b4b0ef6
xmuli <xmulitech@gmail.com>
2020-12-30 16:53:19 +0800

    feat: dde-dock plugin creation
    In DDE, generate a test plugin for dde-dock version 1.2.2 and see if it runs successfully when building a base plugin

--- dde-dock-plugin/NetPlugin.cpp
+++ dde-dock-plugin/NetPlugin.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "NetPlugin.h"
+#include <QLabel>
+
+DWIDGET_USE_NAMESPACE
+
+NetPlugin::NetPlugin(QObject *parent)
+    : QObject(parent)
+    , m_labTest(nullptr)
+{
+
+}
+
+/*!
+ * \brief NetPlugin::pluginName 返回插件名称
+ * \return 插件名（后会更正 MonitorNet）
+ * \note 解决压缩 Bug，在 dde-dock 1.2.3 被修复，故名称叫 “datetime”
+ * \see 参见文末 https://xmuli.tech/posts/c225b552/ 缺陷分析
+ */
+const QString NetPlugin::pluginName() const
+{
+    return "MonitorNet"; //"datetime";
+}
+
+/*!
+ * \brief NetPlugin::init 插件初始化入口函数
+ * \param proxyInter 可认为是主程序的进程
+ */
+void NetPlugin::init(PluginProxyInterface *proxyInter)
+{
+    m_proxyInter = proxyInter;
+    m_labTest = new QLabel("test MonitorNet");
+
+    if (!pluginIsDisable())
+        m_proxyInter->itemAdded(this, pluginName());
+}
+
+/*!
+ * \brief NetPlugin::itemWidget 返回插件主控件,用于显示在 dde-dock 面板上
+ * \param itemKey
+ * \return
+ */
+QWidget *NetPlugin::itemWidget(const QString &itemKey)
+{
+    Q_UNUSED(itemKey)
+    return m_labTest;
+}
--- dde-dock-plugin/NetPlugin.h
+++ dde-dock-plugin/NetPlugin.h
@@ -8,13 +8,28 @@
 #include <QObject>
 #include <pluginsiteminterface.h>
 #include <dtkwidget_global.h>
+#include "../widgets/WinDockNet.h"
 
 DWIDGET_USE_NAMESPACE
 
+class QLabel;
 class NetPlugin : public QObject, public PluginsItemInterface
 {
     Q_OBJECT
+    Q_INTERFACES(PluginsItemInterface)  // 声明实现了的接口
+    Q_PLUGIN_METADATA(IID "com.deepin.dock.PluginsItemInterface" FILE "MonitorNet.json")  // 插件元数据
 
+public:
+    explicit NetPlugin(QObject *parent = nullptr);
+
+    // 插件必须重写函数
+    virtual const QString pluginName() const override;
+    virtual void init(PluginProxyInterface *proxyInter) override;
+    virtual QWidget *itemWidget(const QString &itemKey) override;
+
+private:
+    QLabel *m_labTest;
+    PluginProxyInterface *m_proxyInter;
 };
 
 #endif //LFXNET_NETPLUGIN_H
