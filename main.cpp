b78af82a74470df16eaf71e4d88e74a4e14a8683
xmuli <xmulitech@gmail.com>
2021-01-26 16:26:38 +0800

    feat: Delete test code

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
 #include <QApplication>
-#include <QLoggingCategory>
-#include <QFile>
-#include "widgets/WinSetting.h"
 #include "lib/MonitorInfo_x11.h"
+#include "widgets/WinDockNet.h"
 #include "dde-dock-plugin/WinDockNet.h"
 #include "dde-dock-plugin/WinDdeDockSetting.h"
 
-
-//using namespace lfx;
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    // TEST:
-    WinDdeDockSetting *setting = new WinDdeDockSetting();
-    setting->readConfigWinDdeDock();
-    setting->show();
+//    // TEST:
+//    WinDdeDockSetting *setting = new WinDdeDockSetting();
+//    setting->readConfigWinDdeDock();
+//    setting->show();
 
-//    QFile file("/home/xmuli/.config/lfxNet/config.json");
-//    file.setPermissions(QFile::ReadOwner | QFile::WriteOwner
-//                        | QFile::ReadGroup | QFile::WriteGroup
-//                        | QFile::ReadOther | QFile::WriteOther);
-
+    WinDockNet *dockNet = new WinDockNet();
+    dockNet->show();
 
     return QApplication::exec();
 }
